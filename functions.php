c1ac2b9155ab511b345bad57e058f5337fa28865
Ammad Nazir <ammad.ithj@gmail.com>
2021-03-31 14:40:13 +0500

    update function file
    update function file

--- functions.php
+++ functions.php
@@ -162,8 +162,9 @@ function terrascale_scripts() {
 	wp_enqueue_style( 'Layout', get_template_directory_uri() . '/assets/css/layout.css', array(), _S_VERSION );
 
 	wp_enqueue_script( 'jQuery', get_template_directory_uri() . '/assets/js/jquery-3.5.1.min.js', array(), '3.5.1', true );
-	wp_enqueue_script( 'BootStrap', get_template_directory_uri() . '/assets/js/bootstrap.s.min.js', array(), '5.0.0', true );
+	wp_enqueue_script( 'BootStrap', get_template_directory_uri() . '/assets/js/bootstrap.bundle.min.js', array(), '5.0.0', true );
 	wp_enqueue_script( 'Custom', get_template_directory_uri() . '/assets/js/custom.js', array(), _S_VERSION, true );
+	
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
 	}
