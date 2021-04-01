26588a11219f536cafe3623839fdcf165bb79488
reactive7 <sohaib@reactivespace.com>
2021-04-01 20:06:30 +0500

    blogs-paragraph-trim 2

--- template-parts/content.php
+++ template-parts/content.php
@@ -33,20 +33,7 @@
 
 	<div class="entry-content">
 		<?php
-		the_content(
-			sprintf(
-				wp_kses(
-					/* translators: %s: Name of current post. Only visible to screen readers */
-					__( 'Continue reading<span class="screen-reader-text"> "%s"</span>', 'terrascale' ),
-					array(
-						'span' => array(
-							'class' => array(),
-						),
-					)
-				),
-				wp_trim_words( get_the_content(), 5);
-			)
-		);
+		echo wp_trim_words( get_the_content(), 5);
 
 		wp_link_pages(
 			array(
